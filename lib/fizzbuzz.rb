dd2df8ca4e0c2e439889603312dd805b0bbaf6ab
charles <charlesemery15@gmail.com>
2017-11-15 12:43:50 +0000

    Complete FizzBuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,13 @@
 def fizzbuzz(number)
-  "fizz"
-
+  if number == 0
+    number
+  elsif number%15 == 0
+     "fizzbuzz"
+  elsif number%5 == 0
+     "buzz"
+  elsif number%3 == 0
+     "fizz"
+  else
+    number
+end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,16 @@ describe "fizzbuzz" do
   it "return 'fizz' when passed 3" do
     expect(fizzbuzz(3)).to eq "fizz"
   end
+  it "return 'buzz' when passed 5" do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+  it "return 'fizzbuzz' when passed 15" do
+    expect(fizzbuzz(15).to eq "fizzbuzz")
+  end
+  it "return '8' when passed 8" do
+    expect(fizzbuzz(8).to eq "8")
+  end
+  it "return '0' when passed 0" do
+    expect(fizzbuzz(0). to eq "8")
+  end
 end
